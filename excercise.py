b903af44ad7b7fc9ad891ba7f4285e630ce0c4fb
Paul Dominique Schal <paul.schal@gmx.de>
2020-04-20 02:07:31 +0200

    Full training

--- excercise.py
+++ excercise.py
@@ -14,45 +14,57 @@ class Trainingsplan:
     def result(self):
         return None
 
-    def addExcercise(self):
-        excercise = Excercise()
-        excercise.addSet(Pushup(10,3,3))
-        excercise.addSet(Pushup(10,3,3))
-        excercise.addSet(Pushup(10,3,3))
-        excercise.addSet(Pushup(10,3,3))
+    def addExcercise(self,name = "Excercise", maxReps=10,sets = 4, up = 3, down = 3):
+        excercise = Excercise(name=name)
+        for i in range(sets):
+            excercise.addSet(name=name,maxReps=10,up=up,down=down)
+        self.excercises.append(excercise)
 
-        self.excercises.append(Excercise())
-
-    def startTraining(self):
+    def startTraining(self,pauseDuration = 60):
         beginn = pd.Timestamp(datetime.datetime.now())
         print("Training started at "+str(beginn))
         for excercise in self.excercises:
-            excercise.startExcercise()
+            excercise.startExcercise(pauseDuration)
             
 
 class Excercise:
-    name = " "
+    def __init__(self,name = "Excercise"):
+        self.name = name
     beginn = None
     end = None
     pause = None
     sets = []
+    
+    
 
-    def addSet(self,set):
-        self.sets.append(set)
+    def addSet(self,name = "Excercise",maxReps = 10, up = 3, down = 3):
+        self.sets.append(Set(name = self.name,maxReps = 10, up = 3, down = 3))
+        
 
     def startExcercise(self,pauseDuration = 60):
+        print("Starting  "+self.name)
         for i in range(len(self.sets)):
             self.sets[i].playSet()
             pause(pauseDuration)
             
+            
 class Set:
     def __init__(self,name = "Excercise",maxReps = 10, up = 3, down = 3):
-        self.name = " "
+        self.name = name
         self.maxReps = maxReps
         self.doneReps = 0
+        self.up = up
+        self.down = down
+
     def playSet(self):
         for rep in range(self.maxReps):
             sound.playSound(self.up+self.down)
+        self.getResult()
+    
+    def getResult(self):
+        print("So... how many pushups did you make?")
+        self.doneReps = input()
+
 
 
 class Pushup(Set):
@@ -73,7 +85,7 @@ class Pushup(Set):
         
 
 def pause(length):
-    print("\nWell done... Now rest for "+str(length)+" seconds!")
+    print("Well done... Now rest for "+str(length)+" seconds!")
     for j in range(length):
         sys.stdout.write('\r\a{j}'.format(j=length-j))
         sys.stdout.flush()
@@ -83,5 +95,10 @@ def pause(length):
 ### Testscenario
 if __name__ == "__main__":
     training = Trainingsplan()
-    training.addExcercise()
-    training.startTraining()
+    training.addExcercise(name = "Pushups", maxReps = 10, sets = 4, up = 3, down = 3)
+    training.addExcercise(name = "Situps", maxReps = 10, sets = 4, up = 3, down = 3)
+    training.addExcercise(name = "Planks", maxReps = 10, sets = 4, up = 3, down = 3)
+    training.addExcercise(name = "Rückenzieher", maxReps = 10, sets = 4, up = 3, down = 3)
+    training.addExcercise(name = "Kniebeugen links", maxReps = 10, sets = 3, up = 3, down = 3)
+    training.addExcercise(name = "Kniebeugen rechts", maxReps = 10, sets = 3, up = 3, down = 3)
+    training.startTraining(pauseDuration=60)
